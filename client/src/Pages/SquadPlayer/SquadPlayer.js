c20b092d9241b130cf9f01a26fad198967e8bd11
facumruiz <facundoruiz105@gmail.com>
2023-04-09 14:28:22 -0300

    add table players

--- client/src/Pages/SquadPlayer/SquadPlayer.js
+++ client/src/Pages/SquadPlayer/SquadPlayer.js
@@ -8,267 +8,301 @@ import {
   Button,
   Typography,
 } from "@mui/material";
+
+// Tables imports
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import { Link } from "react-router-dom";
 import "./SquadPlayer.css";
+import { Padding } from "@mui/icons-material";
 
 // List players function
-/* const Record = (props) => (
-  <Grid
-    container
-    marginTop="50px"
-    display="flex"
-    wrap="wrap"
-    justifyContent="center"
-  >
-    <Card
-      sx={{ maxWidth: 200, marginX: 4, marginY: 3 }}
-      className="squad-players-card"
-    >
-      <CardContent className="squad-players-card">
-        <Typography gutterBottom variant="h5" component="div">
+const Record = (props) => (
+  <>
+    {/*  <Grid
+     container
+     marginTop="50px"
+     display="flex"
+     wrap="wrap"
+     justifyContent="center"
+   >
+     <Card
+       sx={{ maxWidth: 200, marginX: 4, marginY: 3 }}
+       className="squad-players-card"
+     >
+       <CardContent className="squad-players-card">
+         <Typography gutterBottom variant="h5" component="div">
+           {props.record.name + " " + props.record.surname}
+         </Typography>
+         <Typography variant="body2" color="#fff">
+           <img src={props.record.na0} alt="Nacionalidad"></img>
+         </Typography>
+         <Typography variant="body2" color="#fff">
+           {props.record.age0}
+         </Typography>
+         <Typography variant="body2" color="#fff">
+           {props.record.pn + " - " + props.record.ps}
+         </Typography>
+       </CardContent>
+       <CardActions className="squad-players-card-actions"></CardActions>
+     </Card>
+   </Grid >*/}
+
+    <TableBody>
+
+      <TableRow
+
+      >
+        <TableCell component="th" scope="row" sx={{ color: 'white' }} as={Link} to={`/detail/${props.record._id}`}>
           {props.record.name + " " + props.record.surname}
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          <img src={props.record.na0} alt="Nacionalidad"></img>
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          {props.record.age0}
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          {props.record.pn + " - " + props.record.ps}
-        </Typography>
-      </CardContent>
-      <CardActions className="squad-players-card-actions"></CardActions>
-    </Card>
-  </Grid>
-); */
+
+        </TableCell>
+        <TableCell align="left">
+          <Button
+
+            as={Link}
+            to={`/edit/${props.record._id}`}
+
+
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+            </svg>
+          </Button>
+        </TableCell>
+        <TableCell align="center" sx={{ color: 'white' }}>{props.record.pn + " - " + props.record.ps}</TableCell>
+        <TableCell align="center" sx={{ color: 'white' }}>{props.record.age0}</TableCell>
+        <TableCell align="center" sx={{ color: 'white' }}><img src={props.record.na0} alt="Nacionalidad"></img></TableCell>
+        <TableCell align="center">
+
+
+          <Button
+
+
+            onClick={() => {
+              props.deleteRecord(props.record._id);
+            }}
+
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-file-earmark-x-fill" viewBox="0 0 16 16">
+              <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM6.854 7.146 8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 1 1 .708-.708z" />
+            </svg>
+          </Button>
+
+        </TableCell>
+      </TableRow>
+
+    </TableBody></>
+);
 
 // List only POR players function
 const RecordPOR = (props) => (
-    <Card
-      sx={{ maxWidth: 200, marginX: 4, marginY: 3 }}
-      className="squad-players-card"
-    >
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="300"
-        width="400"
-        image={props.record.img}
-      />
-      <CardContent className="squad-players-card">
-        <Typography gutterBottom variant="h5" component="div">
-          {props.record.name + " " + props.record.surname}
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          <img src={props.record.na0} alt="Nacionalidad"></img>
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          {props.record.age0}
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          {props.record.pn + " - " + props.record.ps}
-        </Typography>
-      </CardContent>
-      <CardActions className="squad-players-card-actions">
-        <Button
-          as={Link}
-          to={`/detail/${props.record._id}`}
-          className="btn-squad-players"
-          size="small"
-        >
-          Detalles
-        </Button>
-        <Button
-          as={Link}
-          to={`/edit/${props.record._id}`}
-          className="btn-squad-players"
-          size="small"
-        >
-          Editar
-        </Button>
-      </CardActions>
+  <Card
+    sx={{ maxWidth: 200, marginX: 4, marginY: 3 }}
+    className="squad-players-card"
+  >
+
+    <CardContent className="squad-players-card">
+      <Typography gutterBottom variant="h5" component="div">
+        {props.record.name + " " + props.record.surname}
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        <img src={props.record.na0} alt="Nacionalidad"></img>
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        {props.record.age0}
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        {props.record.pn + " - " + props.record.ps}
+      </Typography>
+    </CardContent>
+    <CardActions className="squad-players-card-actions">
       <Button
+        as={Link}
+        to={`/detail/${props.record._id}`}
         className="btn-squad-players"
-        onClick={() => {
-          props.deleteRecord(props.record._id);
-        }}
         size="small"
       >
-        Delete
+        Detalles
       </Button>
-    </Card>
+      <Button
+        as={Link}
+        to={`/edit/${props.record._id}`}
+        className="btn-squad-players"
+        size="small"
+      >
+        Editar
+      </Button>
+    </CardActions>
+    <Button
+      className="btn-squad-players"
+      onClick={() => {
+        props.deleteRecord(props.record._id);
+      }}
+      size="small"
+    >
+      Delete
+    </Button>
+  </Card>
 );
 
 // List only POR players function
 const RecordDEF = (props) => (
-    <Card
-      sx={{ maxWidth: 200, marginX: 4, marginY: 3 }}
-      className="squad-players-card"
-    >
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="300"
-        width="400"
-        image={props.record.img}
-      />
-      <CardContent className="squad-players-card">
-        <Typography gutterBottom variant="h5" component="div">
-          {props.record.name + " " + props.record.surname}
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          <img src={props.record.na0} alt="Nacionalidad"></img>
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          {props.record.age0}
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          {props.record.pn + " - " + props.record.ps}
-        </Typography>
-      </CardContent>
-      <CardActions className="squad-players-card-actions">
-        <Button
-          as={Link}
-          to={`/detail/${props.record._id}`}
-          className="btn-squad-players"
-          size="small"
-        >
-          Detalles
-        </Button>
-        <Button
-          as={Link}
-          to={`/edit/${props.record._id}`}
-          className="btn-squad-players"
-          size="small"
-        >
-          Editar
-        </Button>
-      </CardActions>
+  <Card
+    sx={{ maxWidth: 200, marginX: 4, marginY: 3 }}
+    className="squad-players-card"
+  >
+
+    <CardContent className="squad-players-card">
+      <Typography gutterBottom variant="h5" component="div">
+        {props.record.name + " " + props.record.surname}
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        <img src={props.record.na0} alt="Nacionalidad"></img>
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        {props.record.age0}
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        {props.record.pn + " - " + props.record.ps}
+      </Typography>
+    </CardContent>
+    <CardActions className="squad-players-card-actions">
       <Button
+        as={Link}
+        to={`/detail/${props.record._id}`}
         className="btn-squad-players"
-        onClick={() => {
-          props.deleteRecord(props.record._id);
-        }}
         size="small"
       >
-        Delete
+        Detalles
       </Button>
-    </Card>
+      <Button
+        as={Link}
+        to={`/edit/${props.record._id}`}
+        className="btn-squad-players"
+        size="small"
+      >
+        Editar
+      </Button>
+    </CardActions>
+    <Button
+      className="btn-squad-players"
+      onClick={() => {
+        props.deleteRecord(props.record._id);
+      }}
+      size="small"
+    >
+      Delete
+    </Button>
+  </Card>
 );
 
 // List only MED players function
 const RecordMED = (props) => (
-    <Card
-      sx={{ maxWidth: 200, marginX: 4, marginY: 3 }}
-      className="squad-players-card"
-    >
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="300"
-        width="400"
-        image={props.record.img}
-      />
-      <CardContent className="squad-players-card">
-        <Typography gutterBottom variant="h5" component="div">
-          {props.record.name + " " + props.record.surname}
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          <img src={props.record.na0} alt="Nacionalidad"></img>
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          {props.record.age0}
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          {props.record.pn + " - " + props.record.ps}
-        </Typography>
-      </CardContent>
-      <CardActions className="squad-players-card-actions">
-        <Button
-          as={Link}
-          to={`/detail/${props.record._id}`}
-          className="btn-squad-players"
-          size="small"
-        >
-          Detalles
-        </Button>
-        <Button
-          as={Link}
-          to={`/edit/${props.record._id}`}
-          className="btn-squad-players"
-          size="small"
-        >
-          Editar
-        </Button>
-      </CardActions>
+  <Card
+    sx={{ maxWidth: 200, marginX: 4, marginY: 3 }}
+    className="squad-players-card"
+  >
+
+    <CardContent className="squad-players-card">
+      <Typography gutterBottom variant="h5" component="div">
+        {props.record.name + " " + props.record.surname}
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        <img src={props.record.na0} alt="Nacionalidad"></img>
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        {props.record.age0}
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        {props.record.pn + " - " + props.record.ps}
+      </Typography>
+    </CardContent>
+    <CardActions className="squad-players-card-actions">
       <Button
+        as={Link}
+        to={`/detail/${props.record._id}`}
         className="btn-squad-players"
-        onClick={() => {
-          props.deleteRecord(props.record._id);
-        }}
         size="small"
       >
-        Delete
+        Detalles
       </Button>
-    </Card>
+      <Button
+        as={Link}
+        to={`/edit/${props.record._id}`}
+        className="btn-squad-players"
+        size="small"
+      >
+        Editar
+      </Button>
+    </CardActions>
+    <Button
+      className="btn-squad-players"
+      onClick={() => {
+        props.deleteRecord(props.record._id);
+      }}
+      size="small"
+    >
+      Delete
+    </Button>
+  </Card>
 );
 
 // List only DEL players function
 const RecordDEL = (props) => (
-    <Card
-      sx={{ maxWidth: 200, marginX: 4, marginY: 3 }}
-      className="squad-players-card"
-    >
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="300"
-        width="400"
-        image={props.record.img}
-      />
-      <CardContent className="squad-players-card">
-        <Typography gutterBottom variant="h5" component="div">
-          {props.record.name + " " + props.record.surname}
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          <img src={props.record.na0} alt="Nacionalidad"></img>
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          {props.record.age0}
-        </Typography>
-        <Typography variant="body2" color="#fff">
-          {props.record.pn + " - " + props.record.ps}
-        </Typography>
-      </CardContent>
-      <CardActions className="squad-players-card-actions">
-        <Button
-          as={Link}
-          to={`/detail/${props.record._id}`}
-          className="btn-squad-players"
-          size="small"
-        >
-          Detalles
-        </Button>
-        <Button
-          as={Link}
-          to={`/edit/${props.record._id}`}
-          className="btn-squad-players"
-          size="small"
-        >
-          Editar
-        </Button>
-      </CardActions>
+  <Card
+    sx={{ maxWidth: 200, marginX: 4, marginY: 3 }}
+    className="squad-players-card"
+  >
+
+    <CardContent className="squad-players-card">
+      <Typography gutterBottom variant="h5" component="div">
+        {props.record.name + " " + props.record.surname}
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        <img src={props.record.na0} alt="Nacionalidad"></img>
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        {props.record.age0}
+      </Typography>
+      <Typography variant="body2" color="#fff">
+        {props.record.pn + " - " + props.record.ps}
+      </Typography>
+    </CardContent>
+    <CardActions className="squad-players-card-actions">
       <Button
+        as={Link}
+        to={`/detail/${props.record._id}`}
         className="btn-squad-players"
-        onClick={() => {
-          props.deleteRecord(props.record._id);
-        }}
         size="small"
       >
-        Delete
+        Detalles
       </Button>
-    </Card>
+      <Button
+        as={Link}
+        to={`/edit/${props.record._id}`}
+        className="btn-squad-players"
+        size="small"
+      >
+        Editar
+      </Button>
+    </CardActions>
+    <Button
+      className="btn-squad-players"
+      onClick={() => {
+        props.deleteRecord(props.record._id);
+      }}
+      size="small"
+    >
+      Delete
+    </Button>
+  </Card>
 );
 
 export default function SquadPlayer() {
@@ -305,7 +339,7 @@ export default function SquadPlayer() {
   }
 
   // map data id
-/*   function recordList() {
+  function recordList() {
     return records.map((record) => {
       return (
         <Record
@@ -315,7 +349,7 @@ export default function SquadPlayer() {
         />
       );
     });
-  } */
+  }
 
   // map data id & pn = POR
   function recordListPOR() {
@@ -385,7 +419,23 @@ export default function SquadPlayer() {
   return (
     <>
       <h1>Plantilla</h1>
-{/*       <div>{recordList()}</div> */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 400, backgroundColor: '#190e43' }} aria-label="simple table">
+          <TableHead>
+            <TableRow >
+              <TableCell sx={{ color: 'white' }}>Nombre</TableCell>
+              <TableCell align="left"></TableCell>
+              <TableCell sx={{ color: 'white' }} align="center">Posición</TableCell>
+              <TableCell sx={{ color: 'white' }} align="center">Edad</TableCell>
+              <TableCell sx={{ color: 'white' }} align="center">Nacionalidad</TableCell>
+              <TableCell sx={{ color: 'white' }} align="center">Borrar</TableCell>
+            </TableRow>
+          </TableHead>
+          {recordList()}
+
+        </Table>
+      </TableContainer>
+
 
       <h2>Porteros</h2>
       <Grid
