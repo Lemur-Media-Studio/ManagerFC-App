ae879588982457d717e75d4863f9d9dabd0eadda
facumruiz <facundoruiz105@gmail.com>
2023-04-20 19:23:46 -0300

    fix srv error

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,3 +1,4 @@
+
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
@@ -5,21 +6,21 @@ const client = new MongoClient(Db, {
   useUnifiedTopology: true,
 });
 
-var _db;
+let _db;
 
 module.exports = {
   connectToServer: async function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
-  },
 
+    try {
+      await client.connect();
+    } catch (e) {
+      console.error(e);
+    }
+
+    _db = client.db("employees");
+
+    return (_db === undefined ? false : true);
+  },
   getDb: function () {
     return _db;
   },
