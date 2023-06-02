0102065d0cf62d5c33b2105d55cd5aba2e16dead
Martin del Rosario <delrosariomartin23@gmail.com>
2023-06-02 14:42:28 +0200

    SquadField created and added to SquadPlayer

--- client/src/Pages/Shortlist/Shortlist.jsx
+++ client/src/Pages/Shortlist/Shortlist.jsx
@@ -1,20 +1,5 @@
-import React, { useEffect, useState } from "react";
-import {
-  Button, Container, Grid,
-} from "@mui/material";
-
-// Tables imports
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Link } from "react-router-dom";
+import React from "react";
 import "./Shortlist.css";
-import Loading from "../../Components/Spinner/Spinner";
-import Input from '@mui/material/Input';
 import ShortlistedField from "./ShortlistedField";
 
 export default function Shortlist() {
--- client/src/Pages/Shortlist/ShortlistedField.jsx
+++ client/src/Pages/Shortlist/ShortlistedField.jsx
@@ -10,7 +10,6 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import { Link } from "react-router-dom";
 import "./Shortlist.css";
 import Loading from "../../Components/Spinner/Spinner";
